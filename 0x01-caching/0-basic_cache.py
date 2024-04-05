dcf4080a4b872160c8705ffb3d8dd93ad8213eb1
Atala2000 <atalakidi@gmail.com>
2024-04-06 02:30:13 +0300

    Correction to task 0 and task 1

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -15,6 +15,9 @@ class BasicCache(BaseCaching):
         self.cache_data[key] = item
 
     def get(self, key: str):
+        """
+        Method that retrieves items
+        """
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data.get(key)
