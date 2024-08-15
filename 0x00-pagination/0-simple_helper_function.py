b448d792fe60497f4e44e19eff689ba2c6b61f9b
Atala2000 <atalakidi@gmail.com>
2024-08-15 10:30:12 +0300

    chore: simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Function to return a list of index used for pagination
+    Args:
+        page (int): Current page number
+        page_size (int): Items per page
+    """
     total_pages = page_size * page
     return ((page - 1) * page_size, total_pages)
